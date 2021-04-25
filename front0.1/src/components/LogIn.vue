e22540cb4654888f503815f0734f7fd2bf6b3633
AlexDivs99 <a01367863@itesm.mx>
2021-04-25 18:20:50 -0500

    Catalogos añadidos a index y Login
    Añadí 5 catalogos a index y sus botones en Login

--- front0.1/src/components/LogIn.vue
+++ front0.1/src/components/LogIn.vue
@@ -7,7 +7,12 @@
       <button @click="navCatalogPrices"> Ir al catálogo de Precios </button>
       <button @click="navCatalogClients"> Ir al catálogo de Clientes </button>
       <button @click="navCaptureOrder"> Ir a la captura de Órdenes </button>
-      <button @click="navCaptureOrder"> Ir a la Consulta de Órdenes en proceso </button>
+      <button @click="navOrdersInProcess"> Ir a la Consulta de Órdenes en proceso </button>
+      <button @click="navCatalogAgents"> Ir al catálogo de Agentes </button>
+      <button @click="navCatalogDelivered"> Ir al catálogo de Entregas </button>
+      <button @click="navCatalogFacturas"> Ir al catálogo de Facturas </button>
+      <button @click="navCatalogSaldo"> Ir al catálogo de Saldos </button>
+      <button @click="navCatalogWarehouse"> Ir al catálogo de Almacenes </button>
     </div>
 
 </template>
@@ -32,9 +37,24 @@ export default {
     navCaptureOrder() {
       this.$router.push({name: 'CaptureOrder'})
     },
-    navCaptureOrder() {
+    navOrdersInProcess() {
       this.$router.push({name: 'OrdersInProcess'})
-    }
+    },
+    navCatalogAgents() {
+      this.$router.push({name: 'CatalogAgents'})
+    },
+    navCatalogDelivered() {
+      this.$router.push({name: 'CatalogDelivered'})
+    },
+    navCatalogFacturas() {
+      this.$router.push({name: 'CatalogFacturas'})
+    },
+    navCatalogSaldo() {
+      this.$router.push({name: 'CatalogSaldo'})
+    },
+    navCatalogWarehouse() {
+      this.$router.push({name: 'CatalogWarehouse'})
+    },
     }
   }
 
