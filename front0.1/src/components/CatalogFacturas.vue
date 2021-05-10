cfcc076b0d44e8d0b02c411edcf05b06feaa8c96
SaraSiller97 <60633799+SaraSiller97@users.noreply.github.com>
2021-05-09 23:51:48 -0500

    Update CatalogFacturas.vue

--- front0.1/src/components/CatalogFacturas.vue
+++ front0.1/src/components/CatalogFacturas.vue
@@ -1,6 +1,6 @@
 <template>
-    <div>
-    <h1> Catálogo de Facturas </h1>
+    <div id="test">
+    <h1 id="header1"> Catálogo de Facturas </h1>
     <div class="inputForm">
     <form>
       <label>Fecha</label>
@@ -28,7 +28,7 @@
    <button @click="signUpFactura"> Dar de alta </button>
    <button @click="signDownFactura"> Dar de baja </button>
    <button @click="loadFactura">Actualizar </button>
-   <div style="width: 80%" >
+   <div id="table">
   <vue-table-dynamic :params="params"
       @select="onSelect"
       @selection-change="onSelectionChange"
@@ -112,15 +112,62 @@ export default {
 
 <style scoped>
 .inputForm {
-  width: 300px;
+  width: 400px;
   clear: both;
+  color: #213485;
+  margin: 3%;
 }
 .inputForm  input {
   width: 100%;
   clear: both;
+  margin-top: 2%;
+  margin-bottom: 5%;
+  font-family: "GOTY0", "GOTY1", "GOTY2", verdana;
+  opacity: 50%;
+  border-radius: 6px;
+  border: transparent;
 }
 .inputForm  textarea {
-  width: 100% ;
-  height: 100px;
+  width: 150%;
+  height: 90px;
+  color: #213485;
+  margin-top: 2%;
+  margin-bottom: 0%;
+  font-family: "GOTY0", "GOTY1", "GOTY2", verdana;
+  opacity: 50%;
+  border-radius: 6px;
+  border: transparent;
+}
+button{
+  margin-top: 0%;
+  margin-left: 3%;
+  color: #0E2CA4;
+  opacity: 70%;
+  font-family: "GOTY0", "GOTY1", "GOTY2", verdana;
+  text-shadow: 1px 1px rgba(14,44,164,0.50);
+  background-color: transparent;
+  padding: 5px;
+  font-weight: 700;
+  font-size: 12px;
+  border-radius: 6px;
+  border: transparent;
+}
+button:hover{
+  background-color: rgba(14,44,164,0.30) ;
+}
+#test{
+  background-color: rgba(33,52,133,0.20);
+  margin: 1%;
+  color: #3B0EA4;
+  font-family: "GOTY0", "GOTY1", "GOTY2", verdana;
+}
+#header1{
+  margin: 2%;
+  font-size: 30px;
+}
+#table{
+  width: 80%;
+  margin-left: 10%;
+  margin-top: 2%;
 }
 </style>
