3d8a6ec2b7dcb82173182d1440bdc00000c63891
SaraSiller97 <60633799+SaraSiller97@users.noreply.github.com>
2021-05-10 00:14:01 -0500

    Update OrdersInProcess.vue

--- front0.1/src/components/OrdersInProcess.vue
+++ front0.1/src/components/OrdersInProcess.vue
@@ -1,6 +1,6 @@
 <template>
     <div id="test">
-    <h1> Órdenes de venta en proceso </h1>
+    <h1 id="header1"> Órdenes de venta en proceso </h1>
 
    <button @click="loadOrders">Actualizar</button>
    <div id="table" >
@@ -136,5 +136,10 @@ button{
 
 button:hover{
   background-color: rgba(14,44,164,0.30) ;
+}
+
+#header1{
+  margin: 2%;
+  font-size: 30px;
 }
 </style>
