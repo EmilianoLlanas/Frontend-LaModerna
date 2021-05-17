58fc1039d6ba7c7405819d1f997595935c936cea
jessyGM <jessica.gm.124@gmail.com>
2021-05-16 20:00:53 -0500

    Commiting validation in catalogInventory

--- front0.1/src/components/CatalogInventory.vue
+++ front0.1/src/components/CatalogInventory.vue
@@ -1,21 +1,28 @@
 <template>
     <div>
     <h1> Catálogo de Inventarios </h1>
+    
     <div class="inputForm">
     <form>
-      <label>Compañia</label>
+      <label>Compañía</label>
       <br>
-      <input v-model="invCom" placeholder="Compañía"> 
+      <input id="invCom" v-model="invCom" placeholder="Compañía"> 
       <br>
-      <input v-model="invWarehouse" placeholder="Almacén">
+      <label>Almacén</label>
       <br>
-      <input v-model="invArticle" placeholder="Artículo">
+      <input id="invWarehouse" v-model="invWarehouse" placeholder="Almacén">
       <br>
-      <input v-model="invStock" placeholder="Stock">
+      <label>Artículo</label>
+      <br>
+      <input id="invArticle" v-model="invArticle" placeholder="Artículo">
+      <br>
+      <label>Stock</label>
+      <br>
+      <input id= "invStock" v-model="invStock" type="number" min="0" placeholder="Stock">
    </form>
   </div>
    <br>
-   <button @click="signUpInv"> Agregar </button>
+   <button @click="checkForm"> Agregar </button>
    <button @click="signDownInv"> Eliminar </button>
    <button @click="loadInv">Actualizar </button>
    <button @click="reportInv">Generar reporte </button>
@@ -26,6 +33,11 @@
       ref="table"></vue-table-dynamic>
 
   </div>
+    <div id="error">
+    <ul>
+        <li v-for="error in errors" v-bind:key="error">{{error}}</li>
+      </ul>
+      </div>
   </div>
 </template>
 
@@ -38,14 +50,15 @@ export default {
       invCom:'',
       invWarehouse:'',
       invArticle:'',
-      invStock:'',
+      invStock:0,
       select:'',
+      errors:[],
       params: {
         data: [
           ['Compañía', 'Almacén', 'Artículo', 'Stock'],
-          ['Plásticos de México', 'Blvd. Solidaridad las Torres S/N, San Salvador Tizatlali, 52172 Metepec, Méx.' ,'200109 CORR. MARQUETA 5KG', '4320.0'],
-          ['Barcel', 'Avenida Eduardo Monroy Cárdenas 2000 San Antonio Buenavista, 50110 Toluca de Lerdo, Méx.' ,'200167 CORR. UNIVERSAL GRANEL ', '40.0'],
-          ['Totis', 'Delegación San Buenaventura, 50110 Toluca de Lerdo, Méx.','LAMINA CARTON CH-1 ', '2000.0' ],
+          ['Plásticos de México', 'Blvd. Solidaridad las Torres S/N, San Salvador Tizatlali, 52172 Metepec, Méx.' ,'200109 CORR. MARQUETA 5KG', '4320'],
+          ['Barcel', 'Avenida Eduardo Monroy Cárdenas 2000 San Antonio Buenavista, 50110 Toluca de Lerdo, Méx.' ,'200167 CORR. UNIVERSAL GRANEL ', '40'],
+          ['Totis', 'Delegación San Buenaventura, 50110 Toluca de Lerdo, Méx.','LAMINA CARTON CH-1 ', '2000' ],
         ],
         deleteData:[],
         header: 'row',
@@ -70,20 +83,41 @@ export default {
       this.params.deleteData=checkedIndexs
     },
 
+    checkForm(){
+        this.errors=[];
+        if(this.invCom && this.invWarehouse && this.invArticle && this.invStock){
+          this.signUpInv();
+        }
+        else{
+          alert("Por favor, llene todos los campos correctamente para agregar un registro");
+           if(!this.invCom) 
+          {
+            this.errors.push('Introduce compañía');
+          }
+          if(!this.invWarehouse) 
+          {
+            this.errors.push('Introduce almacén');
+          }
+          if(!this.invArticle) 
+          {
+            this.errors.push('Introduce artículo');
+          }
+          if(!this.invStock) //como es un campo numerico, no recoge nada si llenas con texto
+          {
+            this.errors.push('Stock no es un número valido');
+          }          
+        }
+    },
+
     signUpInv(){
         //there will be a method here to establish connection with backend and sign up the address' data, some day....
-        if(this.invCom==''||this.invWarehouse==''||this.invArticle==''||this.invStock=='')
-        {
-          alert('Por favor, llene todos los campos para registrar inventario')
-        }
-        else
-        {
+
+          //Push data
           this.params.data.push([this.invCom, this.invWarehouse, this.invArticle, this.invStock]);
           this.invCom='';
           this.invWarehouse='';
           this.invArticle='';
           this.invStock='';
-        }
     },
 
     signDownInv(){
