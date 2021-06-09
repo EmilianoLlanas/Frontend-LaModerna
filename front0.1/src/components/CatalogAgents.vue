a1251da7f5d1e0c07b93ff7b7f1d16285882d78d
daniel5115 <daniel811815@gmail.com>
2021-06-09 08:12:02 -0500

    Update

--- front0.1/src/components/CatalogAgents.vue
+++ front0.1/src/components/CatalogAgents.vue
@@ -1,43 +1,62 @@
 <template>
-    <div id="test">
-    <h1 id="header1"> Catálogo de Agentes </h1>
-    <div class="inputForm">
-    <form>
-      <label>Compañia</label>
-      <br>
-      <input v-model="agCom" placeholder="Compañia">
-      <br>
-      <label>ID</label>
-      <br>
-      <input v-model="agId" placeholder="Identificador del agente">
-      <br>
-      <label>Representante</label>
-      <br>
-      <input v-model="rep" placeholder="Representante del agente">
-      <br>
-      <label>Estauts</label>
-      <br>
-      <input v-model="agEst" placeholder="Estatus del agente">
-   </form>
-  </div>
-   <button @click="signUpAgent"> Dar de alta </button>
-   <button @click="signDownAgent"> Dar de baja </button>
-   <button @click="loadAgent">Actualizar </button>
-   <div id="table">
-  <vue-table-dynamic :params="params"
-      @select="onSelect"
-      @selection-change="onSelectionChange"
-      ref="table"></vue-table-dynamic>
 
-  </div>
+  <div id="content">
+
+    <h1 id="header1"> Catálogo de Agentes </h1>
+
+    <div id="card">
+
+      <div id="cardheader"></div>
+
+      <div class="inputForm">
+
+        <div id="error">
+          <ul>
+            <li v-for="error in errors" v-bind:key="error">{{error}}</li>
+          </ul>
+        </div>
+
+        <form>
+          <label>Compañia</label>
+          <br>
+          <input v-model="agCom" placeholder="Compañia">
+          <br>
+          <label>ID</label>
+          <br>
+          <input v-model="agId" placeholder="Identificador del agente">
+          <br>
+          <label>Representante</label>
+          <br>
+          <input v-model="rep" placeholder="Representante del agente">
+          <br>
+          <label>Estatus</label>
+          <br>
+          <input v-model="agEst" placeholder="Estatus del agente">
+        </form>
+
+      </div>
+
+      <div id="buttons">
+        <button @click="checkForm"> Dar de alta </button>
+        <button @click="signDownAgent"> Dar de baja </button>
+        <button @click="loadAgent"> Actualizar </button>
+      </div>
+
+      <div id="table">
+        <vue-table-dynamic :params="params"
+          @select="onSelect"
+          @selection-change="onSelectionChange"
+          ref="table">
+        </vue-table-dynamic>
+        <br>
+      </div>
+
+    </div>
   </div>
 </template>
 
 <script>
 import VueTableDynamic from 'vue-table-dynamic'
-import 'es6-promise/auto'
-import auth from "@/auth";
-
 export default {
   name: 'CatalogAgents',
   data() {
@@ -46,8 +65,6 @@ export default {
       agId:'',
       rep:'',
       agEst:'',
-      error:false,
-      dataTable:'',
       errors:[],
       params: {
         data: [
@@ -75,7 +92,32 @@ export default {
     },
     onSelectionChange (checkedDatas, checkedIndexs, checkedNum) {
       console.log('onSelectionChange: ', checkedDatas, checkedIndexs, checkedNum)
-      this.params.deleteDate=checkedIndexs
+      this.params.deleteData=checkedIndexs
+    },
+    checkForm(){
+      this.errors=[];
+      if(this.agCom && this.agId && this.rep && this.agEst){
+        this.signUpAgent();
+      }
+      else{
+        alert('Por favor, llene todos los campos para registrar al agente');
+        if(!this.agCom)
+        {
+          this.errors.push('Introduce el nombre de la Compañia');
+        }
+        if(!this.agId)
+        {
+          this.errors.push('Introduce un id de Agente');
+        }
+        if(!this.rep)
+        {
+          this.errors.push('Introduce un representante del Agente');
+        }
+        if(!this.agEst)
+        {
+          this.errors.push('Introduce estatus del agente');
+        }
+      }
     },
     signUpAgent(){
         if(this.agCom==''||this.agId==''||this.rep==''||this.agEst=='')
@@ -86,6 +128,10 @@ export default {
         {
           this.params.data.push([this.agCom, this.agId, this.rep, this.agEst]);
         }
+        this.agCom='';
+        this.agId='';
+        this.rep='';
+        this.agEst='';
     },
     signDownAgent(){
       this.agCom='';
@@ -97,24 +143,12 @@ export default {
       this.params.data.splice(this.params.deleteData[i], 1)
       }
     },
-    async loadAgent(){
-      //conexion con Backend
-        // a method here to establish connection with backend and update the table
-      try {
-      console.log(this.$store.getters.token)
-      this.dataTable=((await auth.getAgents(this.$store.getters.token)));
-      console.log(this.dataTable)
-      //this.$router.push("/")
-      } catch (error) {
-        this.error=true;
-        console.log(error);
-      }
+    loadAgent(){
         this.agCom='';
         this.agId='';
         this.rep='';
         this.agEst='';
         alert("Actualizando informacion...");
-
     }
   },
   components: { VueTableDynamic }
@@ -122,23 +156,32 @@ export default {
 </script>
 
 <style scoped>
-.inputForm {
-  width: 400px;
+.inputForm{
+  width: 90%;
   clear: both;
   color: #213485;
   margin: 3%;
+  font-size: 20px;
+  font-family: Verdana;
+  font-size: 20px;
 }
-.inputForm  input {
+.inputForm input{
   width: 100%;
   clear: both;
   margin-top: 2%;
   margin-bottom: 5%;
+  height: 50px;
+  font-size: 20px;
   font-family: "GOTY0", "GOTY1", "GOTY2", verdana;
   opacity: 50%;
   border-radius: 6px;
   border: transparent;
+  background: #f2f2f2;
+  padding: 10px;
+  color: #213485;
 }
-.inputForm  textarea {
+.inputForm textarea{
+  padding: 10px;
   width: 150%;
   height: 90px;
   color: #213485;
@@ -148,6 +191,10 @@ export default {
   opacity: 50%;
   border-radius: 6px;
   border: transparent;
+  background: #f2f2f2;
+  width: 100%;
+  font-family: Verdana;
+  font-size: 20px;
 }
 button{
   margin-top: 0%;
@@ -159,26 +206,51 @@ button{
   background-color: transparent;
   padding: 5px;
   font-weight: 700;
-  font-size: 12px;
+  font-size: 24px;
   border-radius: 6px;
   border: transparent;
+  margin-bottom: 40px;
 }
 button:hover{
-  background-color: rgba(14,44,164,0.30) ;
-}
-#test{
-  background-color: rgba(33,52,133,0.20);
-  margin: 1%;
-  color: #3B0EA4;
-  font-family: "GOTY0", "GOTY1", "GOTY2", verdana;
+  background-color: rgba(14,44,164,0.30);
 }
 #header1{
   margin: 2%;
-  font-size: 30px;
+  font-family: Verdana;
+  font-size: 60px;
+  color: #FFFF;
+  text-align: center;
 }
 #table{
   width: 80%;
   margin-left: 10%;
   margin-top: 2%;
+}
+label{
+  font-family: Verdana;
+  font-weight: bold;
+}
+#card{
+  background: #fff;
+  width: 80%;
+  margin: 5em;
+  -webkit-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.14);
+  -moz-box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.14);
+  box-shadow: 10px 10px 5px 0px rgba(0,0,0,0.14);
+  border-radius: 15px;
+}
+#cardheader{
+  height: 20px;
+  width: 100%;
+  background: #3B0EA4;
+}
+#buttons{
+  width: 100%;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+}
+#error{
+  color: red;
 }
 </style>
